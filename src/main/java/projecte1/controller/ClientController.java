369801c57971286196e283f11c9523be8fabb946
REZU86 <yass.mahmy@yahoo.com>
2022-10-08 20:17:34 +0200

    getclient update code

--- src/main/java/projecte1/controller/ClientController.java
+++ src/main/java/projecte1/controller/ClientController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import projecte1.Objectes.Client;
@@ -13,26 +14,35 @@ import projecte1.Objectes.Client;
 @RestController
 public class ClientController {
 	
-	@GetMapping("hola")
-	public String bonaTarda() {
-		return "<h1>bona tarda</h1>";
-	}
+	ArrayList<Client> clients;
 	
-	@GetMapping("client")
-	public Client getClient() {
-		return new Client("Joana", "Petita", 
-				LocalDate.of(2000, Month.APRIL, 23));
+	@GetMapping("client/{id}")
+	public Client getClient(@PathVariable(required=false,name="id") int id) {
+		return clients.get(id);
 	}
 	
 	@GetMapping("clients")
 	public List<Client> getClients(){
-		ArrayList<Client> clients = new ArrayList<>();
+		clients = new ArrayList<>();
 		
-		clients.add(new Client("Joana", "Petita", 
+		clients.add(new Client("Joana", "Fernando", 
 				LocalDate.of(2000, Month.APRIL, 23)));
-		clients.add(new Client("Joan", "Petit", 
+		clients.add(new Client("Juan", "Giemenez", 
 				LocalDate.of(2000, Month.MARCH, 22)));
-		
+		clients.add(new Client("Paul", "Teresa", 
+				LocalDate.of(2000, Month.MARCH, 22)));
+		clients.add(new Client("David", "Sánchez", 
+				LocalDate.of(2000, Month.MARCH, 22)));
+		clients.add(new Client("Sara", "Gonozales", 
+				LocalDate.of(2000, Month.MARCH, 22)));
+		clients.add(new Client("Carmen", "Perez", 
+				LocalDate.of(2000, Month.MARCH, 22)));
+		clients.add(new Client("Ellie", "Martin", 
+				LocalDate.of(2000, Month.MARCH, 22)));
+		clients.add(new Client("Marco", "Garcia", 
+				LocalDate.of(2000, Month.MARCH, 22)));
+		clients.add(new Client("Jose", "Rordriguez", 
+				LocalDate.of(2000, Month.MARCH, 22)));
 		return clients;
 	}
 
